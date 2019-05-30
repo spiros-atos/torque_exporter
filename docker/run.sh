65aece409c11d49c6d2308e0ce8018d0e9a241c4
spiros.michalakopoulos@atos.net <spiros.michalakopoulos@atos.net>
2019-05-30 12:45:58 +0200

    more tidy up

--- docker/run.sh
+++ docker/run.sh
@@ -1,19 +1,6 @@
 export dockerregistry="registry.test.euxdat.eu/euxdat"
 export service="torque_exporter"
-#export input_folder=$(pwd)/input_test
-#export output_folder=$(pwd)/output
 docker build --rm --force-rm --tag=$service .
 #docker build --no-cache --rm --force-rm --tag=$service .
 docker pull $dockerregistry/$service:latest
-#docker run -v "$input_folder:/var/data" \
-#  -v "$output_folder:/var/output" \
-#  -e INPUT_RASTERFILE=raster_4326.tif \
-#  -e INPUT_SHAPEFILE=vector_4326.shp \
-#  -e OUTPUT_RASTERFILE=raster_4326_output.tif \
-#  -e OUTPUT_STATISTICSFILE=statistics.json \
-#  $service
-#docker rmi -f $service
 docker run $service #-host="hazelhen.hww.de" -ssh-user="xeuspimi" -ssh-password="P1l1nh@01"
-#docker run --rm -d -p 9100 $service -host="hazelhen.hww.de" -ssh-user="xeuspimi" -ssh-password="P1l1nh@01"
-#docker run --rm -d -p 9100 spirosmichalak/torque_exporter $ARGS
-
