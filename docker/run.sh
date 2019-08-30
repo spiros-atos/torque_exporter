b36c289e4acaa783e6bc83668bde728ed6e1f6cf
spiros <spiros.michalakopoulos@atos.net>
2019-08-30 12:56:05 +0200

    changed password

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@ export service="torque_exporter"
 docker build --rm --force-rm --tag=$service .
 #docker build --no-cache --rm --force-rm --tag=$service .
 docker pull $dockerregistry/$service:latest
-docker run $service #-host="hazelhen.hww.de" -ssh-user="xeuspimi" -ssh-password="P1l1nh@01"
+docker run $service
